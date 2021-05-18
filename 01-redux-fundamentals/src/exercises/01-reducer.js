df3a5cd0ac9795aa8288adcf80d1f2944ab78631
Brian Sahota <bpsahota@gmail.com>
2021-05-18 14:13:11 -0400

    redux store

--- 01-redux-fundamentals/src/exercises/01-reducer.js
+++ 01-redux-fundamentals/src/exercises/01-reducer.js
@@ -2,24 +2,43 @@
 
 const initialState = { value: 0 };
 
-// ✅  use initialState as the default value for state if it is undefined when the function is called
 function reducer(state = initialState, action) {
-  if(action.type === "counter/incremented") {
-    return {
-      ...state,
-      value: state.value + 1
-    }
-  } else if(action.type === 'counter/decremented') {
+  switch (action.type) {
+    case "counter/incremented":
       return {
         ...state,
-        value: state.value - 1
-      }
-  } else {
-    return state
+        value: state.value + 1,
+      };
+    case "counter/decremented":
+      return {
+        ...state,
+        value: state.value - 1,
+      };
+    default:
+      return state;
   }
-  // ✅ handle the type "counter/incremented" and add the payload to the state
-  // ✅ handle the type "counter/decremented" and subtract the payload from the state
-  // ✅ handle all other types by returning the state without modifying it
 }
 
 export default reducer;
+
+// ✅  use initialState as the default value for state if it is undefined when the function is called
+// function reducer(state = initialState, action) {
+//   if(action.type === "counter/incremented") {
+//     return {
+//       ...state,
+//       value: state.value + 1
+//     }
+//   } else if(action.type === 'counter/decremented') {
+//       return {
+//         ...state,
+//         value: state.value - 1
+//       }
+//   } else {
+//     return state
+//   }
+  // ✅ handle the type "counter/incremented" and add the payload to the state
+  // ✅ handle the type "counter/decremented" and subtract the payload from the state
+  // ✅ handle all other types by returning the state without modifying it
+// }
+
+// export default reducer;
