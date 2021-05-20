048c01ac121e02f38dd61b3a8c4566f65d929eb3
Brian Sahota <bpsahota@gmail.com>
2021-05-20 16:18:10 -0400

    header done

--- 03-react-redux/src/exercises/components/Header.js
+++ 03-react-redux/src/exercises/components/Header.js
@@ -1,24 +1,19 @@
-import React from "react";
 // 📃 Check out these resources on the useDispatch and useSelector hooks!
 // https://redux.js.org/tutorials/essentials/part-2-app-structure#dispatching-actions-with-usedispatch
 // https://redux.js.org/tutorials/essentials/part-2-app-structure#reading-data-with-useselector
 
+import { toggleDarkMode } from "../redux/themeSlice";
 import { useDispatch, useSelector } from "react-redux";
-
-// ✅ uncomment the line below to import the action you made in the first exercise
-// import { toggleDarkMode } from "../redux/themeSlice";
+import React from "react";
 
 function Header() {
-  // ✅ use the useSelector hook to get the isDarkMode value from Redux state
-  // replace this variable with the value returned by useSelector
-  const isDarkMode = true;
-
-  // ✅ use the useDispatch hook to access the dispatch function
+  const isDarkMode = useSelector((state) => state.theme.isDarkMode)
+  const dispatch = useDispatch()
 
   function handleDarkModeClick() {
-    // ✅ uncomment the line below - this is the action you created in the first exercise!
-    // const action = toggleDarkMode();
-    // ✅ use the dispatch function from useDispatch to send this action to our reducer
+    const action = toggleDarkMode();
+    // console.log(action)
+    dispatch(action)
   }
 
   return (
