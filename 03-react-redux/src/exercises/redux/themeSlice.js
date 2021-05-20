68a3b16871426106cde4e228c5d7c7add71146ba
Brian Sahota <bpsahota@gmail.com>
2021-05-20 14:55:48 -0400

    themeSlice

--- 03-react-redux/src/exercises/redux/themeSlice.js
+++ 03-react-redux/src/exercises/redux/themeSlice.js
@@ -6,12 +6,18 @@ const themeSlice = createSlice({
   name: "theme",
   initialState: {
     // ✅ add an initial state here that will let us toggle dark mode from true to false
+    isDarkMode: false
   },
   reducers: {
     // ✅ add a reducer function that will toggle state from true to false
+    toggleDarkMode(state) {
+      state.isDarkMode = !state.isDarkMode
+    }
   },
 });
 
 // ✅ export the action creator from your reducer function
+
+export const { toggleDarkMode } = themeSlice.actions
 
 export default themeSlice.reducer;
